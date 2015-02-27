2344019d2d19095a96a88138ed5f759211bb7441
Edward Norton <Edward Norton@114.1.4.126>
2015-02-27 16:36:36 +0800

    subset

--- src/QueuesDeques/Subset.java
+++ src/QueuesDeques/Subset.java
@@ -1,10 +1,23 @@
 package QueuesDeques;
-
+import com.algs4.stdlib.StdIn;
 
 public class Subset {
 	
 	public static void main(String[] args){
-		String s= Standard.readString()
-				while (!StdIn.isEmpty()) { s = StdIn.readString(); String s = StdIn.readString(); ... ... }...
+//		String s= StdIn.readString();
+		int k=Integer.valueOf(args[0]);
+		RandomizedQueue<Character> rd=new RandomizedQueue<Character>();
+		while (!StdIn.hasNextChar()){ 
+			char s = StdIn.readChar();
+			if(rd.size()<k){
+				rd.enqueue(s);
+			}else{
+				rd.dequeue();
+				rd.enqueue(s);
+			}		
+		}
+		for(Character c:rd){
+			System.out.println(c.charValue());
+		}
 	}
 }
